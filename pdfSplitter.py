2afe003089ea7bf1d69d761d560296646a5e49f7
yaminocellist <58488672+yaminocellist@users.noreply.github.com>
2024-06-03 22:39:36 +0800

    Add files via upload

---
+++ pdfSplitter.py
@@ -0,0 +1,21 @@
+import os
+from PyPDF2 import PdfReader, PdfWriter
+
+def split_pdf(file, directory, ranges):
+    pdf = PdfReader(file)
+    for i, page_range in enumerate(ranges):
+        pdf_writer = PdfWriter()
+        for page in range(page_range[0], page_range[1]):
+            pdf_writer.add_page(pdf.pages[page])
+
+        output_filename = os.path.join(directory, '{}_part_{}.pdf'.format(
+            os.path.splitext(os.path.basename(file))[0], i+1))
+
+        with open(output_filename, 'wb') as out:
+            pdf_writer.write(out)
+
+        print('Created: {}'.format(output_filename))
+
+# Define the page ranges for each part
+ranges = [(0, 376), (376, 606), (606, 788), (788, 1162)]
+split_pdf('FilesForProcessing/introduction-to-algorithms-3rd-edition.pdf', 'FilesAfterProcessing', ranges)
