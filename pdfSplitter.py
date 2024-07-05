0da1f03974ed93b3424e8989a70fc4a9ab3fd876
yaminocellist <realpklionmagnus@gmail.com>
2024-07-06 03:05:05 +0800

    2nd update

--- pdfSplitter.py
+++ pdfSplitter.py
@@ -17,5 +17,6 @@ def split_pdf(file, directory, ranges):
         print('Created: {}'.format(output_filename))
 
 # Define the page ranges for each part
-ranges = [(0, 376), (376, 606), (606, 788), (788, 1162)]
-split_pdf('FilesForProcessing/introduction-to-algorithms-3rd-edition.pdf', 'FilesAfterProcessing', ranges)
+# ranges = [(0, 376), (376, 606), (606, 788), (788, 1162)]
+ranges = [(0, 162), (162, 334), (334, 525)]
+split_pdf('FilesForProcessing/Wesley_2004.pdf', 'FilesAfterProcessing', ranges)
